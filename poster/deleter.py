9e896159e2363ef8bfdddbc206b2fba097bc993f
Rahul Hoque <rhoque@umich.edu>
2024-07-16 10:23:25 -0400

    this should work

--- poster/deleter.py
+++ poster/deleter.py
@@ -4,3 +4,6 @@ def deleter():
     os.remove('tts_audio.mp3')
     os.remove('brainrot.en.srt')
     os.remove('final_clip_with_subtitles.mp4')
+    os.remove('en_US-lessac-medium.onnx')
+    os.remove('en_US-lessac-medium.onnx.json')
+    os.remove('uploader.py-oauth2.json')
